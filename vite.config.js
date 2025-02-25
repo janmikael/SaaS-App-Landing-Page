505b3c48ca3781b3bd0848384cd553a21617da9f
janmikael <michaelkimu446@gmail.com>
2025-02-26 03:55:57 +0800

    Updated project with React 17 and fixed deployment issues

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/SaaS-App-Landing-Page/" : "/",
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@images": "/images", // Create an alias for the images folder
-    },
-  },
 });
